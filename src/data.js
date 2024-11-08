7e74c857f079ce501d5d53d1e2001fd984810c0d
Eduardo Ajuchán Mazate <eduardoajuchan@gmail.com>
2024-11-07 20:41:03 -0600

    update new products

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ export const productos = [
     id: 1,
     nombre: "Alfajores de fresa",
     categoria: "Comida",
-    precio: 150.0,
+    precio: 48.0,
     descripcion: "Deliciosos alfajores coreanos de fresa con relleno de crema.",
     imagen: "https://i.imgur.com/Rj4WjDF.jpeg", // Ruta desde public
   },
@@ -20,8 +20,8 @@ export const productos = [
     id: 3,
     nombre: "Bolitas de chocolate",
     categoria: "Comida",
-    precio: 300.0,
-    descripcion: "Dolitas de nuez cubiertas de chocolate",
+    precio: 18.0,
+    descripcion: "Bolitas de nuez cubiertas de chocolate",
     imagen: "https://i.imgur.com/JfwFJ8j.jpeg", // Ruta desde public
   },
   {
@@ -36,7 +36,7 @@ export const productos = [
     id: 5,
     nombre: "Dulces de uva",
     categoria: "Dulces",
-    precio: 350.0,
+    precio: 35.0,
     descripcion: "Dulces coreanos con sabor a uva",
     imagen: "https://i.imgur.com/xPyuWBA.jpeg", // Ruta desde public
   },
@@ -44,7 +44,7 @@ export const productos = [
     id: 6,
     nombre: "Galletas de fresa",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 27.0,
     descripcion: "Galletas coreanas con sabor a fresa",
     imagen: "https://i.imgur.com/dIZ69Ig.jpeg", // Ruta desde public
   },
@@ -52,7 +52,7 @@ export const productos = [
     id: 7,
     nombre: "Galletas con chispas de chocolate",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 31.0,
     descripcion: "Galletas coreanas con chispas de chocolate",
     imagen: "https://i.imgur.com/fWFPAzu.jpeg", // Ruta desde public
   },
@@ -60,7 +60,7 @@ export const productos = [
     id: 8,
     nombre: "Honguitos de chocolate",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 18.0,
     descripcion: "Honguitos de chocolate coreanos",
     imagen: "https://i.imgur.com/05P7F21.jpeg", // Ruta desde public
   },
@@ -68,7 +68,7 @@ export const productos = [
     id: 9,
     nombre: "Caramelos de menta",
     categoria: "Dulces",
-    precio: 350.0,
+    precio: 35.0,
     descripcion: "Caramelos coreanos de menta",
     imagen: "https://i.imgur.com/LhjiXz1.jpeg", // Ruta desde public
   },
@@ -76,7 +76,7 @@ export const productos = [
     id: 10,
     nombre: "Mini galletas de chocolate",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 20.0,
     descripcion: "Mini galletas coreanas rellenas de chocolate",
     imagen: "https://i.imgur.com/xLqYDg2.jpeg", // Ruta desde public
   },
@@ -84,105 +84,247 @@ export const productos = [
     id: 11,
     nombre: "Sopa ramen paldo de mariscos (picante)",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 26.0,
     descripcion: "Sopa ramen coreana en tazón de mariscos picante",
     imagen: "https://i.imgur.com/t1wF0yb.jpeg", // Ruta desde public
   },
+  
   {
     id: 12,
-    nombre: "Pastelitos de pez",
-    categoria: "Comida",
-    precio: 350.0,
-    descripcion: "Pastelitos coreanos de pez con relleno de chocolate",
-    imagen: "https://i.imgur.com/NT3JefJ.jpeg", // Ruta desde public
-  },
-  {
-    id: 13,
     nombre: "Dulces coreanos de leche",
     categoria: "Dulces",
-    precio: 350.0,
+    precio: 48.0,
     descripcion: "Deliciosos dulces coreanos de leche",
     imagen: "https://i.imgur.com/mObWeMg.jpeg", // Ruta desde public
   },
   {
-    id: 14,
+    id: 13,
     nombre: "Dulces coreanos de fresa",
     categoria: "Dulces",
-    precio: 350.0,
+    precio: 48.0,
     descripcion: "Deliciosos dulces coreanos de fresa",
     imagen: "https://i.imgur.com/Cqv3ihq.jpeg", // Ruta desde public
   },
   {
-    id: 15,
+    id: 14,
     nombre: "Cheetos coreanos de especias picantes",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 27.0,
     descripcion: "Cheetos coreanos con sabor a especias picantes y queso",
     imagen: "https://i.imgur.com/tSjsDKh.jpeg", // Ruta desde public
   },
   {
-    id: 16,
+    id: 15,
     nombre: "Cheetos coreanos de barbacoa",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 27.0,
     descripcion: "Deliciosos Cheetos coreanos de barbacoa y queso",
     imagen: "https://i.imgur.com/B0ebLcD.jpeg", // Ruta desde public
   },
   {
-    id: 17,
+    id: 16,
     nombre: "Sopa ramen paldo de verduras (picante)",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 26.0,
     descripcion: "Deliciosa sopa ramen coreana en tazón de verduras picante",
     imagen: "https://i.imgur.com/9fgJ9al.jpeg", // Ruta desde public
   },
   {
-    id: 18,
+    id: 17,
     nombre: "Sopa ramen paldo de res (picante)",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 26.0,
     descripcion: "Deliciosa sopa ramen coreana en tazón de res picante",
     imagen: "https://i.imgur.com/l3hz53C.jpeg", // Ruta desde public
   },
+ 
   {
-    id: 19,
-    nombre: "Snack coreano de camote",
-    categoria: "Comida",
-    precio: 350.0,
-    descripcion: "Delicioso snack coreano de camote",
-    imagen: "https://i.imgur.com/8i4nSsN.jpeg", // Ruta desde public
-  },
-  {
-    id: 20,
+    id: 18,
     nombre: "Pocky Pepero (variedad de sabores)",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 24.0,
     descripcion: "Deliciosos palitos de galleta coreanos con variedad de sabores, Fresa, chocolate, almendra, crunchy",
     imagen: "https://i.imgur.com/PAAFUbn.jpeg", // Ruta desde public
   },
+  
   {
-    id: 21,
-    nombre: "Sopa ramen de res (tazón)",
-    categoria: "Comida",
-    precio: 350.0,
-    descripcion: "Deliciosa sopa ramen coreana en tazón de res",
-    imagen: "https://i.imgur.com/zHC8955.jpeg", // Ruta desde public
-  },
-  {
-    id: 22,
-    nombre: "Snacks papitas de camote",
-    categoria: "Comida",
-    precio: 350.0,
-    descripcion: "Deliciosos snacks coreanos de papitas de camote",
-    imagen: "https://i.imgur.com/6nZvKtU.jpeg", // Ruta desde public
-  },
-  {
-    id: 23,
+    id: 19,
     nombre: "Sopa ramen de res y verduras (tazón)",
     categoria: "Comida",
-    precio: 350.0,
+    precio: 20.0,
     descripcion: "Deliciosa sopa ramen coreana en tazón de res y verduras",
     imagen: "https://i.imgur.com/Xf7X23Q.jpeg", // Ruta desde public
   },
+  {
+    id: 20,
+    nombre: "Crema de arroz",
+    categoria: "Skincare",
+    precio: 35.0,
+    descripcion: "Crema de arroz para el rostro",
+    imagen: "https://i.imgur.com/ES9GLdI.jpeg", // Ruta desde public
+  },
+  {
+    id: 21,
+    nombre: "Crema depiladora (aroma a rosas)",
+    categoria: "Cuidado personal",
+    precio: 25.0,
+    descripcion: "Crema depiladora, suave y no causa irritación, especialmente diseñado para pieles delicadas, elimna el vello grueso",
+    imagen: "https://i.imgur.com/ngxXGE2.jpeg", // Ruta desde public
+  },
+  {
+    id: 22,
+    nombre: "Protector Solar CeraVe",
+    categoria: "Skincare",
+    precio: 96.0,
+    descripcion: "Protector solar CeraVe, FPS 50, no deja sensación grasosa, protege de los rayos UVA y UVB",
+    imagen: "https://i.imgur.com/EbvxQ4h.jpeg", // Ruta desde public
+  },
+  {
+    id: 23,
+    nombre: "Espuma limpiadora BioLand",
+    categoria: "Skincare",
+    precio: 70.0,
+    descripcion: "Espuma limpiadora BioLand, limpia y purifica la piel, elimina impurezas y maquillaje",
+    imagen: "https://i.imgur.com/gjglMmm.jpeg", // Ruta desde public
+  },
+  {
+    id: 24,
+    nombre: "Exfoliante corporal Luckylily",
+    categoria: "Cuidado personal",
+    precio: 20.0,
+    descripcion: "Exfoliante corporal Luckylily, elimina células muertas, suaviza y humecta la piel, de nuez y sal marina",
+    imagen: "https://i.imgur.com/L26LHNM.jpeg", // Ruta desde public
+  },
+  {
+    id: 25,
+    nombre: "Crema de manos",
+    categoria: "Cuidado personal",
+    precio: 15.0,
+    descripcion: "Crema de manos, hidrata y suaviza la piel, aroma a lavanda y aloe vera",
+    imagen: "https://i.imgur.com/XwT7btc.jpeg", // Ruta desde public
+  },
+  {
+    id: 26,
+    nombre: "Agua micelar 5 en 1",
+    categoria: "Skincare",
+    precio: 44.0,
+    descripcion: "Agua micelar, limpia y desmaquilla la piel, no necesita enjuague, para todo tipo de piel",
+    imagen: "https://i.imgur.com/iWHaDGL.jpeg", // Ruta desde public
+  },
+  {
+    id: 27,
+    nombre: "Corrector 3 en 1",
+    categoria: "Maquillaje",
+    precio: 20.0,
+    descripcion: "Corrector 3 en 1, cubre imperfecciones, ojeras y manchas, no deja sensación grasosa",
+    imagen: "https://i.imgur.com/Gbzmfsx.jpeg", // Ruta desde public
+  },
+  {
+    id: 28,
+    nombre: "Paleta de sombras 12 colores",
+    categoria: "Maquillaje",
+    precio: 25.0,
+    descripcion: "Paleta de sombras 12 colores, tonos mate y brillantes, larga duración, no se corre",
+    imagen: "https://i.imgur.com/1teGnHa.jpeg", // Ruta desde public
+  },
+  {
+    id: 29,
+    nombre: "Desodorante y antitranspirante Degree",
+    categoria: "Cuidado personal",
+    precio: 35.0,
+    descripcion: "Desodorante y antitranspirante Degree, aroma a leche y avena, protección por 48 horas",
+    imagen: "https://i.imgur.com/GZm4QG1.jpeg", // Ruta desde public
+  },
+  {
+    id: 30,
+    nombre: "Balsamos labiales Vaseline",
+    categoria: "Maquillaje",
+    precio: 15.0,
+    descripcion: "Balsamos labiales Vaseline, hidrata y protege los labios, varios sabores",
+    imagen: "https://i.imgur.com/NAOEkrE.jpeg", // Ruta desde public
+  },
+  {
+    id: 31,
+    nombre: "Gel hidratante Vitamina C PONDS",
+    categoria: "Skincare",
+    precio: 60.0,
+    descripcion: "Gel hidratante Vitamina C PONDS, hidrata y suaviza la piel, protege de los rayos UV y combate las manchas de la piel",
+    imagen: "https://i.imgur.com/7oFeruR.jpeg", // Ruta desde public
+  },
+  {
+    id: 32,
+    nombre: "Gel hidratante de Sandía PONDS",
+    categoria: "Skincare",
+    precio: 60.0,
+    descripcion: "Gel hidratante de Sandía PONDS, hidrata y suaviza la piel, tonifica y refresca la piel",
+    imagen: "https://i.imgur.com/mT7Tw2t.jpeg", // Ruta desde public
+  },
+  {
+    id: 33,
+    nombre: "Gel limpiador vitamina C PONDS",
+    categoria: "Skincare",
+    precio: 70.0,
+    descripcion: "Gel limpiador vitamina C PONDS, limpia y purifica la piel, elimina impurezas e hidrata la piel",
+    imagen: "https://i.imgur.com/1MReSaX.jpeg", // Ruta desde public
+  },
+  {
+    id: 34,
+    nombre: "Kit PONDS VITAMINA C",
+    categoria: "Skincare",
+    precio: 150.0,
+    descripcion: "Kit PONDS VITAMINA C, incluye gel limpiador y gel hidratante, hidrata y suaviza la piel, protege de los rayos UV y combate las manchas de la piel",
+    imagen: "https://i.imgur.com/ZsjhSLr.jpeg", // Ruta desde public
+  },
+  {
+    id: 35,
+    nombre: "Gel hidratante aloevera PONDS",
+    categoria: "Skincare",
+    precio: 60.0,
+    descripcion: "Gel limpiador de Aloe Vera PONDS, limpia y purifica la piel, elimina impurezas e hidrata la piel",
+    imagen: "https://i.imgur.com/XAkEh7i.jpeg", // Ruta desde public
+  },
+  {
+    id: 37,
+    nombre: "Bebida coreana de aloevera",
+    categoria: "Bebidas",
+    precio: 12.0,
+    descripcion: "Bebida coreana de aloevera, hidrata y refresca",
+    imagen: "https://i.imgur.com/7SvELPe.jpeg", // Ruta desde public
+  },
+  {
+    id: 37,
+    nombre: "Bebida coreana en bolsa citricos",
+    categoria: "Bebidas",
+    precio: 14.0,
+    descripcion: "Bebida coreana en bolsa sabor a citricos (Mandarina, Naranja y naranja verde)",
+    imagen: "https://i.imgur.com/q5XEGuE.jpeg", // Ruta desde public
+  
+},
+  {
+    id: 38,
+    nombre: "Bebida coreana en botella (diferentes sabores)",
+    categoria: "Bebidas",
+    precio: 16.0,
+    descripcion: "Bebida coreana en botella de diferentes sabores (Mango, durazno y naranja)",
+    imagen: "https://i.imgur.com/4x49CHh.jpeg", // Ruta desde public
+  },
+  {
+    id: 39,
+    nombre: "Bebida coreana uva y coco (cocopalm)",
+    categoria: "Bebidas",
+    precio: 12.0,
+    descripcion: "Bebida coreana sabor a uva y coco",
+    imagen: "https://i.imgur.com/23L8kX5.jpeg", // Ruta desde public
+  },
+  {
+    id: 40,
+    nombre: "Bebida coreana coco, yogurt y durazno (cocopalm)",
+    categoria: "Bebidas",
+    precio: 10.0,
+    descripcion: "Bebida coreana sabor a coco, yogurt y durazno",
+    imagen: "https://i.imgur.com/MvItaxD.jpeg", // Ruta desde public
+  },
+  
+ 
   
 ];
