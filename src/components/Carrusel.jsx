707e60fbbc2f6e24d3d7fe28fa7367b141a7f512
Eduardo Ajuchán Mazate <eduardoajuchan@gmail.com>
2024-11-11 20:26:35 -0600

    first promo

--- src/components/Carrusel.jsx
+++ src/components/Carrusel.jsx
@@ -3,6 +3,7 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import imagen1 from '../assets/1.png';
 import imagen2 from '../assets/2.png';
 import imagen3 from '../assets/3.png';
+import imagen4 from '../assets/Promo1.png';
 
 function Carrusel() {
   return (
@@ -15,7 +16,7 @@ function Carrusel() {
     interval={3000}
   >
     <div>
-      <img src={imagen1} alt="Imagen 1" />
+      <img src={imagen4} alt="Imagen 1" />
     </div>
     <div>
       <img src={imagen2} alt="Imagen 2" />
